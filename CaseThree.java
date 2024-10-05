9f4411fb7a5c4575aeaeb7b891f20c2f717cb1c7
rendyelang <lsmnrndy123@gmail.com>
2024-10-05 09:46:12 +0700

    case three solution added

---
+++ CaseThree.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class CaseThree {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        
+        // while (true) {
+            System.out.println("Enter the jersey number of the player: ");
+            int jerseyNumber = scan.nextInt();
+            String playerPosition = identifyPlayer(jerseyNumber);
+            System.out.println(playerPosition);
+            System.out.println();
+            scan.close();
+        // }
+    }
+
+    protected static String identifyPlayer(int jerseyNumber) {
+        String playerPosition = "";
+
+        Boolean isTargetAttacker = jerseyNumber %2 == 0;
+        Boolean isRightCaptain = false;
+        if (isTargetAttacker) {
+            isRightCaptain = jerseyNumber >= 50 && jerseyNumber <= 100;
+            if (isRightCaptain) {
+                playerPosition = "The player is Target Attacker and have the right to be Captain";
+            } else {
+                playerPosition = "The player is Target Attacker";
+            }
+        }
+
+        Boolean isDefender = jerseyNumber %2 == 1;
+        Boolean isPlayMaker = false;
+        Boolean isKeeper = false;
+        if (isDefender) {
+            isPlayMaker = jerseyNumber > 90;
+            isKeeper = jerseyNumber %3 == 0 && jerseyNumber %5 == 0;
+            if (isPlayMaker) {
+                playerPosition = "The player is Defender and Play Maker";
+            } else if (isKeeper) {
+                playerPosition = "The player is Defender and Keeper";
+            } else {
+                playerPosition = "The player is Defender";
+            }
+        }
+
+        return playerPosition;
+    }
+}
